e575cf4fa5c9fb431f71fb344979fca3ee0f420b
Adam Moore <adamoore@gmail.com>
2019-08-22 12:44:26 -0400

    working proto

--- js/index.js
+++ js/index.js
@@ -1,4 +1,32 @@
-window.onload = function firstTone() {
-  const synth = new Tone.Synth().toMaster();
-  synth.triggerAttackRelease('C4', '8n');
+let synth = new Tone.Synth().toMaster();
+// window.onload = function firstTone() {
+//   synth.triggerAttackRelease('C4', '8n');
+// };
+
+document.querySelector('#c').onmouseover = function() {
+  synth.triggerAttackRelease('C3', '8n');
+};
+
+document.querySelector('#d').onmouseover = function() {
+  synth.triggerAttackRelease('D3', '8n');
+};
+
+document.querySelector('#e').onmouseover = function() {
+  synth.triggerAttackRelease('E3', '8n');
+};
+
+document.querySelector('#f').onmouseover = function() {
+  synth.triggerAttackRelease('F3', '8n');
+};
+
+document.querySelector('#g').onmouseover = function() {
+  synth.triggerAttackRelease('G3', '8n');
+};
+
+document.querySelector('#a').onmouseover = function() {
+  synth.triggerAttackRelease('A3', '8n');
+};
+
+document.querySelector('#b').onmouseover = function() {
+  synth.triggerAttackRelease('B3', '8n');
 };
