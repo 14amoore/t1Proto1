191415b8a45b35b845cb389db02483e75ecb222d
Adam Moore <adamoore@gmail.com>
2019-08-23 13:09:51 -0400

    added button for better experience.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 let synth = new Tone.Synth().toMaster();
-// window.onload = function firstTone() {
-//   synth.triggerAttackRelease('C4', '8n');
-// };
+window.onload = function firstTone() {
+  synth.triggerAttackRelease('C4', '8n');
+};
+
+document.querySelector('#play').addEventListener('click', () => Tone.start());
 
 document.querySelector('#c').onmouseover = function() {
   synth.triggerAttackRelease('C3', '8n');
